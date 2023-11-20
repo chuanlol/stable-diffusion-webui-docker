d3ff840034a6d0b2301e38eac998f234b66fdea3
chuan <User@DESKTOP-0MMEM1D>
2023-11-20 23:11:18 +0800

    add gif2gif to extensions

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -68,7 +68,7 @@ list=(./extensions/*/install.py)
 for installscript in "${list[@]}"; do
   EXTNAME=$(echo $installscript | cut -d '/' -f 3)
   # Skip installing dependencies if extension is disabled in config
-  if $(jq -e ".disabled_extensions|any(. == \"$EXTNAME\")" config.json); then
+  if $(jq -e ".disabled_extensions|any(. == \"$EXTNAME\")" config.json) | $EXTNAME == "sd-webui-reactor"; then
     echo "Skipping disabled extension ($EXTNAME)"
     continue
   fi
